f3eec148d7f31c552cc3cf46fb6a09de3e7ab079
Brett Henderson <bretth18@gmail.com>
2024-09-06 20:38:10 -0700

    Initial Commit

---
+++ scrobble/scrobbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrobbleApp.swift
+//  scrobble
+//
+//  Created by Brett Henderson on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct scrobbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
